c6066f117f7c9e31d4c8812ffc42fab45e717a20
ana clara <108944811+ana-1g@users.noreply.github.com>
2024-05-23 08:09:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botoes = document.querySelectorAll(".botao")
 console.log(botoes)
 for (let i = 0; 1 < botoes.length ; i++){
-    console.log(botoes[i]) {
-        for (j=0;<botoes.length;j++){
+    botoes[i].onclick =  function(){
+        for (let j = 0 ; j < botoes.length ; j++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
