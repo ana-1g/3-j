c6400981bbebb36bfa715a4bff65a16813cdaa4c
ana clara <108944811+ana-1g@users.noreply.github.com>
2024-07-05 09:46:47 -0300

    05-07

--- main.js
+++ main.js
@@ -6,24 +6,29 @@ const tempoObjetivo1= new Date(2024,8,11,22);
 const tempoObjetivo2= new Date(2024,9,12,21);
 const tempoObjetivo3= new Date(2024,10,13,20);
 const tempoObjetivo4= new Date(2024,11,14,19);
-let segundos;
-let minutos;
-let horas;
-let dias;
+
+function calculaTempo(tempoObjetivo){
 const agora = new Date()
-segundos = (tempoObjetivo1-agora)/1000 
-minutos = segundos/60
-horas = minutos/60
-dias = horas/24
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor(horas);
-dias = Math.floor(dias);
+let segundos
+let minutos
+let horas
+let dias
+segundos = Math.floor((tempoObjetivo-agora)/1000)
+minutos = Math.floor(segundos/60)
+horas = Math.floor(minutos/60)
+dias = Math.floor(minutos/60)
 segundos = segundos%60
 minutos = minutos%60
 horas = horas%60
-tempo[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-console.log(tempo);
+
+return `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+}
+
+tempo[0].textContent = calculaTempo(tempoObjetivo1)
+tempo[1].textContent = calculaTempo(tempoObjetivo2)
+tempo[2].textContent = calculaTempo(tempoObjetivo3)
+tempo[3].textContent = calculaTempo(tempoObjetivo4)
+
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
